fbbdda9e63c7864219d4cd0b3f45ca7a4e4c4e52
Knolle2204 <niklas.knoll@tobit.software>
2018-09-13 14:15:13 +0200

    Add Department model

---
+++ Model/Department.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompanyStructuresWebAPI.Model
+{
+    public class Department
+    {
+        public int Id { get; set; }
+
+        public string DepartmentName { get; set; }
+
+        public string CompanyName { get; set; }
+    }
+}
